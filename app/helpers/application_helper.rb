dce08fb67d4c66fab76523af805aeacae8645110
Michael Hartl <michael@michaelhartl.com>
2010-08-14 16:54:25 -0700

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
