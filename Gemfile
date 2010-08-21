ff5ec335606ec0953db867047be3228c4f111af5
Michael Hartl <michael@michaelhartl.com>
2010-08-21 13:47:33 -0700

    Created a User model

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
 group :development do
   gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'annotate-models'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+# Schema version: 20100821203213
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+end
