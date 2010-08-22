b244aa091d6fab45a1f5dd4c60d6817cf9b1c284
Michael Hartl <michael@michaelhartl.com>
2010-08-21 19:57:29 -0700

    Added a stub User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
   def new
     @title = "Sign up"
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
-
+  resources :users
+  
   root :to => "pages#home"
 
   match '/contact', :to => 'pages#contact'
