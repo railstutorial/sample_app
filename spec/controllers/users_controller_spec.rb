8753262ee86e931178438df985b150f0529c118d
Michael Hartl <michael@michaelhartl.com>
2010-08-22 18:27:41 -0700

    Added a sidebar and some styling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -33,6 +33,12 @@ describe UsersController do
       get :show, :id => @user
       response.should have_selector('h1>img', :class => "gravatar")
     end
+    
+    it "should have the right URL" do
+      get :show, :id => @user
+      response.should have_selector('td>a', :content => user_path(@user),
+                                            :href    => user_path(@user))
+    end
   end
 
   describe "GET 'new'" do
