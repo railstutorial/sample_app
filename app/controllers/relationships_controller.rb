c36e7b16e9a1b33c475cf14a3462626dcb513786
MarsuBoss <marsuboss@gmail.com>
2010-09-02 10:43:14 +0200

    respond_to :html, :js

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,17 @@
 class RelationshipsController < ApplicationController
   before_filter :authenticate
   
+  respond_to :html, :js
+  
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with(@user)
   end
   
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    respond_with(@user)
   end
 end
