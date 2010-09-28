cd648f9bb168f7ef042ac1f307a72edc2e2481dd
Michael Hartl <michael@michaelhartl.com>
2010-09-28 15:46:34 -0700

    Improved test_sign_in

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -13,8 +13,8 @@
 class Relationship < ActiveRecord::Base
   attr_accessible :followed_id
   
-  belongs_to :follower, :foreign_key => "follower_id", :class_name => "User"
-  belongs_to :followed, :foreign_key => "followed_id", :class_name => "User"
+  belongs_to :follower, :class_name => "User"
+  belongs_to :followed, :class_name => "User"
   
   validates :follower_id, :presence => true
   validates :followed_id, :presence => true
