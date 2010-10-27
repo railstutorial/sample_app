d5d8f3573b125456fa6d191024b7b747806935cc
Michael Hartl <michael@michaelhartl.com>
2010-10-26 18:35:19 -0700

    Made some Sessions helper methods private

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,27 +35,27 @@ module SessionsHelper
     redirect_to signin_path, :notice => "Please sign in to access this page."
   end
   
-  def store_location
-    session[:return_to] = request.fullpath
-  end
-  
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     clear_return_to
   end
   
-  def clear_return_to
-    session[:return_to] = nil
-  end
-
   private
-  
+
     def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
     end
     
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
 
