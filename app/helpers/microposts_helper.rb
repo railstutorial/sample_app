337c1deaa36531c3c63973d0a8cf700c0ba80aa7
Michael Hartl <michael@michaelhartl.com>
2011-02-18 19:36:23 -0800

    Added a wrap method

---
+++ app/helpers/microposts_helper.rb
@@ -0,0 +1,15 @@
+module MicropostsHelper
+
+  def wrap(content)
+    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+  end
+
+  private
+
+    def wrap_long_string(text, max_width = 30)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text : 
+                                  text.scan(regex).join(zero_width_space)
+    end
+end
