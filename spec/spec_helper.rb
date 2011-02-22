572ec86da3017043cdf254d3134088245c3192e0
Michael Hartl <michael@michaelhartl.com>
2011-02-21 20:03:02 -0800

    New Spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'spork'
 
 Spork.prefork do
@@ -6,15 +5,13 @@ Spork.prefork do
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
   ENV["RAILS_ENV"] ||= 'test'
-  unless defined?(Rails)
-    require File.dirname(__FILE__) + "/../config/environment"
-  end
+  require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
 
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
-  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  
   Rspec.configure do |config|
     # == Mock Framework
     #
@@ -32,11 +29,6 @@ Spork.prefork do
     # instead of true.
     config.use_transactional_fixtures = true
 
-    ### Part of a Spork hack. See http://bit.ly/arY19y
-    # Emulate initializer set_clear_dependencies_hook in 
-    # railties/lib/rails/application/bootstrap.rb
-    ActiveSupport::Dependencies.clear
-    
     def test_sign_in(user)
       controller.sign_in(user)
     end
