0d1c5daf48a2d26a6c378c9b9fd2dfe9765a3272
Michael Hartl <michael@michaelhartl.com>
2011-11-16 21:14:43 -0800

    Fix a Gravatar XSS vulnerability

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.name,
+    gravatar_image_tag(user.email.downcase, :alt => h(user.name),
                                             :class => 'gravatar',
                                             :gravatar => options)
   end
