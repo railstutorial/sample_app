624db347b7dc51f469fac7b697d646043ed93de4
Michael Hartl <michael@michaelhartl.com>
2012-03-24 21:06:59 -0700

    Add following/followers to before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate,
+                :only => [:index, :edit, :update, :destroy,
+                          :followers, :following]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
   
