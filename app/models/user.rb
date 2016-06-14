d07f595b1a8cc0f31b1cf421cb093a4319ae811d
Namit Khullar <namit_khullar@hotmail.co.uk>
2016-06-14 15:05:04 -0400

    What happens

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@
 class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
-  
+
   has_many :microposts,    :dependent => :destroy
   has_many :relationships, :dependent => :destroy,
                            :foreign_key => "follower_id"
@@ -26,9 +26,9 @@ class User < ActiveRecord::Base
   has_many :following, :through => :relationships, :source => :followed
   has_many :followers, :through => :reverse_relationships,
                        :source  => :follower
-  
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
   validates :email, :presence   => true,
@@ -39,23 +39,23 @@ class User < ActiveRecord::Base
                        :length => { :within => 6..40 }
 
   before_save :encrypt_password
-  
+
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
-  
+
   def feed
     Micropost.from_users_followed_by(self)
   end
-  
+
   def following?(followed)
     relationships.find_by_followed_id(followed)
   end
-  
+
   def follow!(followed)
     relationships.create!(:followed_id => followed.id)
   end
-  
+
   def unfollow!(followed)
     relationships.find_by_followed_id(followed).destroy
   end
@@ -65,28 +65,28 @@ class User < ActiveRecord::Base
       user = find_by_email(email)
       (user && user.has_password?(submitted_password)) ? user : nil
     end
-    
+
     def authenticate_with_salt(id, cookie_salt)
       user = find_by_id(id)
       (user && user.salt == cookie_salt) ? user : nil
     end
   end
-  
+
   private
-  
+
     def encrypt_password
       self.salt = make_salt unless has_password?(password)
       self.encrypted_password = encrypt(password)
     end
-  
+
     def encrypt(string)
       secure_hash("#{salt}--#{string}")
     end
-    
+
     def make_salt
       secure_hash("#{Time.now.utc}--#{password}")
     end
-    
+
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
     end
