2b3a075e81ce1852e5fd15138372e0c536bf7c00
Liu <liuzhidong_1985@hotmail.com>
2022-08-26 16:29:20 +0800

    test codeql

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+
+CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     'abcdefg',                        # required
+      aws_secret_access_key: '1234566778',                        # required
+      region:                '*****',                  # optional, defaults to 'us-east-1'
+    }
+  end
